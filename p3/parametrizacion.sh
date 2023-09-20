a90315f06ac0ed6ad09a563a2a1dfea5fa0f1d0a
Valeria Micol García <122756188+valemicolgarcia@users.noreply.github.com>
2023-09-19 19:07:47 -0500

    parametrizacion - $# - $* - $? - $0 - $1 ...

---
+++ p3/parametrizacion.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#parametrizacion
+echo $# #numero total de argumentos pasados al script
+echo $* #todos los argumentos pasados al script
+echo $? #estado de salida del ultimo comando ejecutado
+echo "El nombre del script es $0 "
+echo "El primer parametro es $1 "
+echo "El segundo parametro es $2 "
+
+
