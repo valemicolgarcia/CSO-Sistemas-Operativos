d766968678659b5fe6a75903ca1f785ca867ff9c
Valeria Micol García <122756188+valemicolgarcia@users.noreply.github.com>
2023-09-20 20:41:08 -0500

    select - ls - pwd - who

---
+++ p3/punto13_b.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+cat << EOF > coment.txt
+
+Uso de las estructuras de control:
+
+(b) Crear un script que muestre 3 opciones al usuario: Listar, DondeEstoy y QuienEsta. Según la opción elegida se le debe mostrar:
+Listar: lista el contenido del directoria actual.
+DondeEstoy: muestra el directorio donde me encuentro ubicado.
+QuienEsta: muestra los usuarios conectados al sistema.
+
+EOF
+
+select opcion in Listar DondeEstoy QuienEsta
+do
+	case $opcion in 
+	"Listar")
+	ls
+	break
+	;;
+	"DondeEstoy")
+	pwd
+	break
+	;;
+	"QuienEsta")
+	who
+	break
+	;;
+	*)
+	echo "No se eligio ninguna de las opciones"
+	break
+	esac
+
+done
